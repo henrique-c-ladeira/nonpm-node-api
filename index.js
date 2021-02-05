c292c126b5798a479dc6746a66be36adfaa0b65c
Henrique Curzio Ladeira <henrique.c.ladeira@gmail.com>
2021-02-04 21:33:01 -0300

    init: http server REST

---
+++ index.js
@@ -0,0 +1,60 @@
+const http = require('http');
+const { StringDecoder } = require('string_decoder');
+const url = require('url');
+
+const server = http.createServer((req,res) => {
+
+  const parsedUrl = url.parse(req.url, true);
+  const trimmedPath = parsedUrl.pathname.replace(/^\/+|\/+$/g, '');
+  const queryStringObject = parsedUrl.query;
+  const method = req.method.toLowerCase();
+  const headers = req.headers;
+
+  const decoder = new StringDecoder('utf-8');
+  let buffer = '';
+  req.on('data', (data) => buffer += decoder.write(data));
+  req.on('end', () => {
+    buffer += decoder.end();
+    const payload = buffer;
+
+    const chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+    const data = {
+      trimmedPath,
+      queryStringObject,
+      method,
+      headers,
+      payload,
+    };
+
+    chosenHandler(data, (statusCode, payload) => {
+      statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+      payload = typeof(payload) == 'object' ? payload : {};
+      const payloadString = JSON.stringify(payload);
+
+      res.setHeader('Content-Type','application/json');
+      res.writeHead(statusCode);
+      res.end(payloadString);
+      console.log(trimmedPath,statusCode);
+    })
+
+  })
+
+});
+
+server.listen(3000, () => "Oh GEE I'm up!");
+
+const handlers = {};
+
+handlers.hello = (data,callback) => {
+  callback(200, {message: 'Hi, do you wanna develop an app?'});
+}
+
+
+handlers.notFound = (data,callback) => {
+  callback(404);
+}
+
+
+router = {
+  hello: handlers.hello,
+}
