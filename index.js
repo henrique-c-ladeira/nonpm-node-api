3004bf74a5731f4a2cffc7df4693f69898874983
Henrique Curzio Ladeira <henrique.c.ladeira@gmail.com>
2021-03-06 23:40:33 -0300

    feat: add command line interface

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const server = require('./lib/server');
+const cli = require('./lib/cli');
 
-server.listen(3000, () => console.log("Oh GEE I'm up!"));
+app = {};
+
+app.init = () => {
+  server.listen(3000, () => console.log("Oh GEE I'm up!"));
+
+  setTimeout(() => cli.init(),50);
+}
+
+app.init();
